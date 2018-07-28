43734ab5ddd9d741ef84c6d6ae61e2637b485129
nchong128 <nchong128@gmail.com>
2018-07-28 23:16:33 +1000

    Allowed for video to pop up

---
+++ indigo/index.js
@@ -0,0 +1,10 @@
+function beginTime() {
+    console.log("start");
+    let button = document.getElementById("time");
+    let video = document.getElementById("video");
+
+    button.style.display = 'none';
+    video.style.display = 'block';
+
+    console.log("end");
+}
